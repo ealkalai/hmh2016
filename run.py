f26b7b284c78bbd23996843ac3240ee9347aa2d5
Jens Panneel <jens.panneel@ing.nl>
2016-03-22 21:19:31 +0100

    post response

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for, send_file
+from flask import Flask, request, Response, render_template, redirect, url_for, send_file, json
 from flask.ext.qrcode import QRcode
 import json
 
 import cStringIO, qrcode
-import json
+import pprint
 
 application = Flask(__name__)
 QRcode(application)
@@ -16,17 +16,31 @@ def login():
 
 @application.route("/qrcode", methods=['GET', 'POST'])
 def thanks():
-    data = ing_create_challenge()
-    return render_template('data.html',d=data)
+    response = ing_create_challenge()
+    data = response.get_data()
+    return render_template('data.html', d=data)
 
-@application.route("/response", methods=['POST'])
-def post_response():
+@application.route("/response", methods=['GET'])
+def get_response():
     #verify device/pin
     #do callback
     return 200
 
+@application.route("/callback", methods=['GET'])
+def handle_callback():
+    return Response(status=200)
+
+@application.route("/response", methods=['POST'])
+def post_response():
+    #verify device/pin
+    data = request.get_json()
+    pprint.pprint(data)
+    handle_callback()
+    return Response(json.dumps(data), status=200, mimetype='application/json')
+
 def ing_create_challenge():
-    return '{url: "http://google.com", date: 2016, uuid: 123}'
+    data = {"url" : "http://google.com", "date" : 2016, "uuid" : 123}
+    return Response(json.dumps(data), status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
