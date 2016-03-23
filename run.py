4c5c20adcd5698fb74debeee554ab64411e039d6
Jens Panneel <jens.panneel@ing.nl>
2016-03-23 01:34:58 +0100

    random uuid

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import random
 import cStringIO, qrcode
 import pprint
 import uuid
+from datetime import datetime
 
 application = Flask(__name__)
 QRcode(application)
@@ -24,9 +25,12 @@ def login():
 
 @application.route("/qrcode", methods=['GET', 'POST'])
 def qr_page():
-    data = ing_create_challenge()
-    d = data.get_data()
-    return render_template('data.html',d=d)
+    text = '{"text": "Login to Mijn ING","extra_parameters": {}}'
+    data = ing_create_challenge(json.loads(text))
+    #data = ing_create_challenge({"text":"Sign in at website","extra_vars":{}})
+    d = json.loads(data.get_data())
+    pprint.pprint(d)
+    return render_template('data.html',d=d,uuid=d['uuid'])
 
 @application.route("/loggedin", methods=['GET', 'POST'])
 def logged_in():
@@ -34,7 +38,9 @@ def logged_in():
 
 @application.route("/response", methods=['GET'])
 def get_response():
-    if "123" in pending_stuff:
+    rv = request.args.get('uuid')
+    pprint.pprint(rv)
+    if rv in pending_stuff:
         return '{"url": "http://google.com"}'
     else:
         return 'not yet'
@@ -57,11 +63,16 @@ def post_response():
     handle_callback(data)
     return Response(json.dumps(data), status=200, mimetype='application/json')
 
-def ing_create_challenge():
-    random_something = "123"
+def ing_create_challenge(data_in):
+    pprint.pprint(data_in)
+    #random_something = "123"
+    #date = str(2016)
+    date = str(datetime.now())
     #random_something = str(uuid.uuid4())
-    data = {"url" : "http://google.com", "date" : 2016, "uuid" : random_something}
-    return Response(json.dumps(data), status=200, mimetype='application/json')
+    random_something = str(random.randint(0,100))
+    data_out = {"text" : data_in['text'], "date" : date, "uuid" : random_something, "extra_parameters" : data_in['extra_parameters']}
+    pprint.pprint(data_out)
+    return Response(json.dumps(data_out), status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
